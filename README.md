b6116e4f4008e9a32cc0d4e3b079744a875d4637
Apostolis Konoupakis <konoupakis@gmail.com>
2016-03-18 21:51:56 +0200

    jsdoc

--- README.md
+++ README.md
@@ -5,8 +5,11 @@
 ![BOWER VERSION](https://img.shields.io/bower/v/object-scrubber.svg)
 ![DOWNLOADS](https://img.shields.io/npm/dt/object-scrubber.svg)
 [![ISSUES](https://img.shields.io/github/issues-raw/akonoupakis/object-scrubber.svg)](https://github.com/akonoupakis/object-scrubber/issues)
-[![Build Status](https://api.travis-ci.org/akonoupakis/object-scrubber.svg?branch=master)](http://travis-ci.org/akonoupakis/object-scrubber)
 ![LICENCE](https://img.shields.io/npm/l/object-scrubber.svg)
+
+[![BUILD](https://api.travis-ci.org/akonoupakis/object-scrubber.svg?branch=master)](http://travis-ci.org/akonoupakis/object-scrubber)
+![STANDARDJS](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)
+[![DEPENDENCIES](https://david-dm.org/akonoupakis/object-scrubber.svg)](https://david-dm.org/akonoupakis/object-scrubber)
 
 [![NPM](https://nodei.co/npm/object-scrubber.png?downloads=true)](https://nodei.co/npm/object-scrubber/)
 
@@ -16,65 +19,11 @@ A simple recursive utility to scrub deep into objects and change their property 
 
 ## usage
 
-```js
-var Scrubber = require('object-scrubber');
-    
-var testObj = {
-    _id: 'test',
-    title: 'test',
-    collection: ['test1', 'test2']    
-};
-        
-var scrubber = new Scrubber();
+* [Getting started](https://cdn.rawgit.com/akonoupakis/object-scrubber/master/docs/jsdoc/tutorial-getting-started.html)
+* [Changing property names](https://cdn.rawgit.com/akonoupakis/object-scrubber/master/docs/jsdoc/tutorial-changing-property-names.html)
+* [Changing property values](https://cdn.rawgit.com/akonoupakis/object-scrubber/master/docs/jsdoc/tutorial-changing-property-values.html)
+* [Recurring scrubbing](https://cdn.rawgit.com/akonoupakis/object-scrubber/master/docs/jsdoc/tutorial-recurring-scrubbing.html)
 
-// replacing "_id" properties to "id"
-scrubber.when(function (x) {
-    return x.parent && x.key === '_id';
-}, function (x) {
-    x.parent.id = x.value;
-    delete x.parent._id;
-    x.key = 'id';
-    return x.scrub(x.parent.id);
-});
+## copyright and license
 
-// scrubbing recuring objects and arrays
-scrubber.when(function (x) {
-    return typeof(x.value) === 'object';
-}, function (x) {
-    return x.scrub(x.value);
-});
-
-// changing string values
-scrubber.when(function (x) {
-    return typeof(x.value) === 'string';
-}, function (x) {
-    return x.value + '_processed!';
-});
-
-scrubber.scrub(testObj);
-```
-
-## license
-```
-The MIT License (MIT)
-
-Copyright (c) 2016 akon
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-```
+Code and documentation copyright 2016 akon. Code released under [the MIT license](https://cdn.rawgit.com/akonoupakis/object-scrubber/master/LICENSE).
--- object-scrubber.js
+++ object-scrubber.js
@@ -1,106 +1,143 @@
-﻿(function () {
-    var _;
+﻿var _
 
-    if (typeof (window) !== 'undefined')
-        _ = window._;
-    else
-        _ = require("underscore");
+/**
+ * Represents the Scrubber class.
+ * @constructor
 
+* @property {array} processors - The processors injected
+*/
+var Scrubber = function () {
+  this.processors = []
+}
 
-    var Scrubber = function () {
-        this.processors = [];
-    };
+/**
+   * Set a processor.
 
-    Scrubber.prototype.when = function (condition, fn) {
-        if (typeof (condition) === 'function' && typeof (fn) === 'function') {
-            this.processors.push({
-                condition: condition,
-                fn: fn
-            });
+   * @param {function} condition - The condition passing a {@link Condition} instance as an argument (should return true/false)
+   * @param {function} trigger - The trigger passing a {@link Trigger} instance as an argument (should return the scrubbed result)
+   */
+Scrubber.prototype.when = function (condition, trigger) {
+  if (typeof (condition) === 'function' && typeof (trigger) === 'function') {
+    this.processors.push({
+      condition: condition,
+      fn: trigger
+    })
+  }
+}
+
+/**
+ * Scrub a target object.
+ */
+Scrubber.prototype.scrub = function (obj) {
+  var self = this
+
+  var result = obj
+
+  if (_.isArray(obj)) {
+    _.each(obj, function (x, i) {
+      if (x) {
+        var scrubbed = self.scrub(x)
+        obj[i] = scrubbed || x
+      }
+    })
+  } else if (_.isObject(obj)) {
+    for (var objKey in obj) {
+      var key = objKey
+
+      var oflagCtx = new Condition(self, obj, key, obj[key])
+      var oprocessCtx = new Trigger(self, obj, key, obj[key])
+
+      _.each(self.processors, function (processor) {
+        if (oflagCtx.value !== undefined && processor.condition(oflagCtx)) {
+          var res = processor.fn(oprocessCtx)
+          key = oprocessCtx.key
+          if (res !== undefined) {
+            obj[key] = res
+          }
         }
-    };
+      })
+    }
+    result = obj
+  } else {
+    var flagCtx = new Condition(self, null, null, obj)
+    var processCtx = new Trigger(self, null, null, obj)
 
-    Scrubber.prototype.scrub = function (obj) {
-        var self = this;
-
-        var result = obj;
-
-        if (_.isArray(obj)) {
-            _.each(obj, function (x, i) {
-                if (x) {
-                    var scrubbed = self.scrub(x);
-                    obj[i] = scrubbed || x;
-                }
-            });
+    _.each(self.processors, function (processor) {
+      if (flagCtx.value !== undefined && processor.condition(flagCtx)) {
+        var res = processor.fn(processCtx)
+        if (res !== undefined) {
+          processCtx.value = res
         }
-        else if (_.isObject(obj)) {
-            for (var objKey in obj) {
+      }
+    })
 
-                var key = objKey;
+    result = processCtx.value
+  }
 
-                var flagCtx = {
-                    parent: obj,
-                    key: key,
-                    value: obj[key]
-                };
+  return result
+}
 
-                var processCtx = {
-                    parent: obj,
-                    key: key,
-                    value: obj[key],
-                    scrub: function (value) {
-                        self.scrub(value);
-                    }
-                }
+/**
+ * Represents the condition function passed on the Scrubber.when.
+ * @constructor
 
-                _.each(self.processors, function (processor) {
-                    if (flagCtx.value !== undefined && processor.condition(flagCtx)) {
-                        var res = processor.fn(processCtx);
-                        key = processCtx.key;
-                        if (res !== undefined)
-                            obj[key] = res;
-                    }
-                });
-            }
-            result = obj;
-        }
-        else {
-            var flagCtx = {
-                parent: null,
-                key: null,
-                value: obj
-            };
+ * @param {Scrubber} sender - The sender scrubber
+ * @param {object} parent - The parent object
+ * @param {string} key - The property key
+ * @param {any} value - The property value
 
-            var processCtx = {
-                parent: null,
-                key: null,
-                value: obj,
-                scrub: function (value) {
-                    self.scrub(value);
-                }
-            }
+ * @property {object} parent - The parent object
+ * @property {string} key - The property key
+ * @property {any} value - The property value
+ */
+var Condition = function (sender, parent, key, value) {
+  this.scrubber = sender
+  this.parent = parent
+  this.key = key
+  this.value = value
+}
 
-            _.each(self.processors, function (processor) {
-                if (flagCtx.value !== undefined && processor.condition(flagCtx)) {
-                    var res = processor.fn(processCtx);
-                    if (res !== undefined) {
-                        processCtx.value = res;
-                    }
-                }
-            });
+/**
+ * Represents the trigger function passed on Scrubber.when.
+ * @constructor
 
-            result = processCtx.value;
-        }
+ * @param {Scrubber} sender - The sender scrubber
+ * @param {object} parent - The parent object
+ * @param {string} key - The property key
+ * @param {any} value - The property value
 
-        return result;
-    };
+ * @property {object} parent - The parent object
+ * @property {string} key - The property key
+ * @property {any} value - The property value
+ */
+var Trigger = function (sender, parent, key, value) {
+  this.scrubber = sender
+  this.parent = parent
+  this.key = key
+  this.value = value
+}
 
-    global = Scrubber;
+/**
+ * Trigger a scrub
 
-    if (module)
-        module.exports = Scrubber;
+ * @param {any} value - A value
+ */
+Trigger.prototype.scrub = function (value) {
+  this.scrubber.scrub(value)
+}
 
-    if (typeof (window) !== 'undefined')
-        window.ObjectScrubber = Scrubber;
+;(function () {
+  if (typeof (window) !== 'undefined') {
+    _ = window._
+  } else {
+    _ = require('underscore')
+  }
 
-}());
+  if (module) {
+    module.exports = Scrubber
+  }
+
+  if (typeof (window) !== 'undefined') {
+    window.ObjectScrubber = Scrubber
+  }
+}())
